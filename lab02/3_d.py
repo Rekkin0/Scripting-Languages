ba6e2eb1e1fe4e81228e0ae224b2b0254bef5506
Rekkin0 <korzenmichal5@gmail.com>
2023-03-15 10:06:41 +0100

    sus

--- lab02/3_d.py
+++ lab02/3_d.py
@@ -3,20 +3,21 @@ from retrieve_data import retrieve_data
 
 def reduce_image_ratio() -> float:
     
-    total_resource_count = 0
     image_count = 0
     image_formats = (".gif", ".jpg", ".jpeg", ".xbm")
+    other_resource_count = 0
     data = retrieve_data().splitlines(True)
     for line in data:
         
         try:
             if line.split()[6].endswith(image_formats):
                 image_count += 1
-            total_resource_count += 1
+            else:
+                other_resource_count += 1
         except:
             continue
             
-    return round(image_count / total_resource_count, 2)
+    return round(image_count / other_resource_count, 2)
 
 
 if __name__ == "__main__":
