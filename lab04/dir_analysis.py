ab39a305ca2c889da953325fe25671574a202ea3
Rekkin0 <korzenmichal5@gmail.com>
2023-03-31 08:29:14 +0200

    lab04 3 done(?)

--- lab04/dir_analysis.py
+++ lab04/dir_analysis.py
@@ -1,8 +1,11 @@
 import sys, subprocess, csv
 from pathlib import Path
+from collections import Counter
+from _collections_abc import Generator
+from txt_analysis import DictAnalysis
 
 
-def get_files(dir: Path):
+def get_files(dir: Path) -> Generator[Path, None, None]:
     for file in dir.iterdir():
         if file.is_dir():
             yield from get_files(file)
@@ -12,15 +15,31 @@ def get_files(dir: Path):
             continue
 
 
-if __name__ == "__main__":
-    
-    dir = Path(sys.argv[1]).expanduser().resolve()
+def process_files(dir: Path) -> list[DictAnalysis]:
     keys = ('filepath', 'char_count', 'word_count', 'line_count', 'most_common_char', 'most_common_word')
     results = []
-    
     for file in get_files(dir):
-        output = subprocess.run(["python3", "txt_analysis.py"], input=str(file), text=True, capture_output=True).stdout
-        reader = csv.DictReader(output, fieldnames=keys, delimiter="\t")
-        #results.append()
+        raw_output = subprocess.run(["python3", "txt_analysis.py"], input=str(file), text=True, capture_output=True).stdout
+        output = csv.DictReader(raw_output.splitlines(), fieldnames=keys, delimiter="\t")
+        results.append(next(output))
         
-    print(*results, sep="\n")
+    return results
+    
+    
+def calculate_properties(results: list[DictAnalysis]) -> tuple:
+    file_count = len(results)
+    char_count = sum(int(result['char_count']) for result in results) # type: ignore
+    word_count = sum(int(result['word_count']) for result in results) # type: ignore
+    line_count = sum(int(result['line_count']) for result in results) # type: ignore
+    most_common_char = str(Counter(result['most_common_char'] for result in results).most_common(1)[0][0]) # type: ignore
+    most_common_word = str(Counter(result['most_common_word'] for result in results).most_common(1)[0][0]) # type: ignore
+    
+    most_common_char = "'" + most_common_char + "'"
+    most_common_word = "'" + most_common_word + "'"
+    return file_count, char_count, word_count, line_count, most_common_char, most_common_word
+
+
+if __name__ == "__main__":
+    dir = Path(sys.argv[1]).expanduser().resolve()
+    results = process_files(dir)
+    print(*calculate_properties(results), sep="\t")
--- lab04/txt_analysis.py
+++ lab04/txt_analysis.py
@@ -1,29 +1,23 @@
 import csv, sys
 from pathlib import Path
 from collections import Counter
+from typing import Any
 
 
-if __name__ == "__main__":
-    
-    filepath = Path(input())
-    with open(filepath, "r") as file:
+DictAnalysis = dict[str, Path | int | str]
+
+
+def analyze_file(filepath: Path) -> DictAnalysis:
+    with open(filepath, 'r') as file:
         text = file.read()
     
     char_count = len(text)
-    
-    words = text.split()
-    word_count = len(words)
-    
+    word_count = len(words := text.split())
     line_count = len(text.splitlines())
+    most_common_char = Counter(text).most_common(1)[0][0]
+    most_common_word = Counter(words).most_common(1)[0][0]
     
-    char_occurences = Counter(text)
-    # char_occurences.pop(' ')
-    most_common_char = char_occurences.most_common(1)[0][0]
-    
-    word_occurences = Counter(words)
-    most_common_word = word_occurences.most_common(1)[0][0]
-    
-    result = {
+    return {
         'filepath': filepath,
         'char_count': char_count,
         'word_count': word_count,
@@ -31,10 +25,11 @@ if __name__ == "__main__":
         'most_common_char': most_common_char,
         'most_common_word': most_common_word,
     }
-
-    writer = csv.DictWriter(sys.stdout, fieldnames=result.keys(), delimiter="\t")
-    writer.writerow(result)
-    # print(*result.values(), sep="\t")
     
-    #/home/rekkin/text/file1
+
+if __name__ == "__main__":
+    filepath = Path(input()).expanduser().resolve()
+    result = analyze_file(filepath)
+    writer = csv.DictWriter(sys.stdout, fieldnames=result.keys(), delimiter='\t')
+    writer.writerow(result)
     
