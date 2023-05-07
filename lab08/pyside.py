8db4ddf986a931a5ee4696ffd6638810b3aa04c5
Rekkin0 <korzenmichal5@gmail.com>
2023-05-07 14:04:32 +0200

    aa

--- lab08/pyside.py
+++ lab08/pyside.py
@@ -8,9 +8,9 @@ from file_manip import get_lines
 class LogViewer(QtW.QMainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
         self.log_file: str
         self.log_lines: list[str]
+        self.initUI()
         
     def initUI(self):
         """
@@ -22,34 +22,45 @@ class LogViewer(QtW.QMainWindow):
 
         # Create the central widget and layout
         central_widget = QtW.QWidget(self)
-        layout = QtW.QGridLayout(central_widget)
+        grid = QtW.QGridLayout(central_widget)
         self.setCentralWidget(central_widget)
-
-        # Add the Open button to the layout
-        open_button = QtW.QPushButton('Open')
-        open_button.clicked.connect(self.show_log_file)
-        layout.addWidget(open_button, 0, 1)
         
         # Add the text box with the file path to the layout
         self.file_path_box = QtW.QLineEdit()
         self.file_path_box.setReadOnly(True)
-        layout.addWidget(self.file_path_box, 0, 0)
+        grid.addWidget(self.file_path_box, 0, 0, 1, 2)
+
+        # Add the Open button to the layout
+        open_button = QtW.QPushButton('Open')
+        open_button.clicked.connect(self.show_logs)
+        grid.addWidget(open_button, 0, 2)
 
         # Add the list box to the layout
         self.list_box = QtW.QListWidget()
-        self.list_box.itemClicked.connect(self.show_log_detail)
-        layout.addWidget(self.list_box, 1, 0)
+        self.list_box.itemClicked.connect(self.display_log_details)
+        grid.addWidget(self.list_box, 1, 0)
 
         # Add the detail box to the layout
         self.detail_box = QtW.QTextEdit()
         self.detail_box.setReadOnly(True)
-        layout.addWidget(self.detail_box, 1, 1)
+        grid.addWidget(self.detail_box, 1, 1, 1, 2)
+        
+        self.previous_button = QtW.QPushButton('Previous')
+        self.previous_button.clicked.connect(self.display_previous_log)
+        grid.addWidget(self.previous_button, 2, 0)
+        
+        self.next_button = QtW.QPushButton('Next')
+        self.next_button.clicked.connect(self.display_next_log)
+        grid.addWidget(self.next_button, 2, 1)
 
-    def show_log_file(self):
-        # Prompt the user to select a log file to open
+    def show_logs(self):
+        # Prompt the user to select a log file to open and get the log lines from the file
         log_file, _ = QtW.QFileDialog.getOpenFileName(self, 'Open Log File', filter='Log Files (*.log)')
         log_lines = get_lines(log_file)
         self.file_path_box.setText(log_file)
+        
+        # Clear the list box and populate it with the log lines
+        self.list_box.clear()
         for line in log_lines:
             if line := line.strip():
                 self.list_box.addItem(line)
@@ -58,10 +69,18 @@ class LogViewer(QtW.QMainWindow):
         self.log_file = log_file
         self.log_lines = log_lines
 
-    def show_log_detail(self, item: QtW.QListWidgetItem):
+    def display_log_details(self, item: QtW.QListWidgetItem):
         # Get the index of the selected item and display the corresponding line from the log file in the detail box
-        index: int = self.list_box.row(item)
-        self.detail_box.setText(self.log_lines[index])
+        self.current_index: int = self.list_box.row(item)      
+        self.detail_box.setText(self.log_lines[self.current_index])
+        
+    def display_previous_log(self):
+        previous_log = self.list_box.item(self.current_index - 1)
+        self.display_log_details(previous_log)
+        
+    def display_next_log(self):
+        next_log = self.list_box.item(self.current_index + 1)
+        self.display_log_details(next_log)
 
 
 def main():
